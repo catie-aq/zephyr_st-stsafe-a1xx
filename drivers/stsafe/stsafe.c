c876b0436b49d2c76ad0d503adcb15b50520da15
Benjamin Grolleau <benjamin.grolleau@outlook.com>
2026-05-20 15:04:04 +0200

    add stsafe-a110 compatibilitie
    Signed-off-by: Benjamin Grolleau <benjamin.grolleau@outlook.com>

--- drivers/stsafe/stsafe.c
+++ drivers/stsafe/stsafe.c
@@ -7,7 +7,6 @@
 #include <zephyr/logging/log.h>
 
 #include "stselib.h"
-#include "drivers/stsafe.h"
 
 LOG_MODULE_REGISTER(stsafe, CONFIG_STSAFE_LOG_LEVEL);
 
@@ -15,18 +14,23 @@ struct stsafe_config {
 	struct i2c_dt_spec i2c;
 	struct gpio_dt_spec reset_gpio;
 	int bus_id;
+	uint8_t device_type;
 };
 
 struct stsafe_data {
 	stse_Handle_t handle;
 	struct k_mutex lock;
 	bool ready;
-	int bus_id;
 };
 
 static int stsafe_reset(const struct device *dev)
 {
 	const struct stsafe_config *cfg = dev->config;
+	int ret = gpio_pin_configure_dt(&cfg->reset_gpio, GPIO_OUTPUT_INACTIVE);
+	if (ret != 0) {
+		LOG_ERR("Reset GPIO config failed: %d", ret);
+		return ret;
+	}
 
 	gpio_pin_set_dt(&cfg->reset_gpio, 1);
 	k_msleep(1);
@@ -69,7 +73,6 @@ static int stsafe_init(const struct device *dev)
 		return -ENODEV;
 	}
 
-	gpio_pin_configure_dt(&cfg->reset_gpio, GPIO_OUTPUT_INACTIVE);
 	k_mutex_init(&data->lock);
 
 	stsafe_reset(dev);
@@ -80,7 +83,9 @@ static int stsafe_init(const struct device *dev)
 		return -EIO;
 	}
 
-	data->handle.io.busID = data->bus_id;
+	data->handle.io.busID = cfg->bus_id;
+	data->handle.device_type = cfg->device_type;
+
 	rc = stse_init(&data->handle, (void *)dev);
 	if (rc != STSE_OK) {
 		LOG_ERR("stse_init failed: 0x%x", rc);
@@ -88,9 +93,13 @@ static int stsafe_init(const struct device *dev)
 	}
 
 	data->ready = true;
-	LOG_INF("STSAFE @0x%02x initialized", cfg->i2c.addr);
+	LOG_INF("STSAFE @0x%02x initialized. Type: %s", cfg->i2c.addr,
+		cfg->device_type == STSAFE_A110 ? "A110" : "A120");
 	return 0;
 }
+
+#define GET_STSAFE_TYPE(inst)                                                                      \
+	COND_CODE_1(DT_INST_NODE_HAS_COMPAT(inst, st_stsafe_a110), (STSAFE_A110), (STSAFE_A120))
 
 #define STSAFE_INIT(inst)                                                                          \
 	static struct stsafe_data stsafe_data_##inst;                                              \
@@ -98,8 +107,16 @@ static int stsafe_init(const struct device *dev)
 		.i2c = I2C_DT_SPEC_INST_GET(inst),                                                 \
 		.reset_gpio = GPIO_DT_SPEC_INST_GET(inst, reset_gpios),                            \
 		.bus_id = inst,                                                                    \
+		.device_type = GET_STSAFE_TYPE(inst),                                              \
 	};                                                                                         \
 	DEVICE_DT_INST_DEFINE(inst, stsafe_init, NULL, &stsafe_data_##inst, &stsafe_cfg_##inst,    \
 			      POST_KERNEL, CONFIG_STSAFE_INIT_PRIORITY, NULL);
 
+#undef DT_DRV_COMPAT
+#define DT_DRV_COMPAT st_stsafe_a120
 DT_INST_FOREACH_STATUS_OKAY(STSAFE_INIT)
+#undef DT_DRV_COMPAT
+
+#define DT_DRV_COMPAT st_stsafe_a110
+DT_INST_FOREACH_STATUS_OKAY(STSAFE_INIT)
+#undef DT_DRV_COMPAT

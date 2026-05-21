3fc08fb4f8803c9061cc257cde0968e14207871b
Benjamin Grolleau <benjamin.grolleau@outlook.com>
2026-05-21 14:09:45 +0200

    fix tester sample to use driver
    Signed-off-by: Benjamin Grolleau <benjamin.grolleau@outlook.com>

--- samples/zephyr_st-stsafe-a1xx-tester/src/main.c
+++ samples/zephyr_st-stsafe-a1xx-tester/src/main.c
@@ -4,15 +4,13 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
-#include "stselib.h"
+LOG_MODULE_REGISTER(main);
+
+#include <drivers/stsafe.h>
 
 #include "helpers/command_decoder.h"
-
-LOG_MODULE_REGISTER(main);
 
 int main(void)
 {
@@ -22,62 +20,59 @@ int main(void)
 
 	int ret = STSE_OK;
 
-#ifdef CONFIG_STSAFE_A110
-	LOG_RAW("Using STSafe-A110 chip type.\n\n");
-#elif defined(CONFIG_STSAFE_A120)
-	LOG_RAW("Using STSafe-A120 chip type.\n\n");
-#else
-	LOG_ERR("Unsupported STSafe chip type: %d", STSAFE_CHIP_TYPE);
-	return -1;
-#endif
-
-	stse_Handle_t stse_handler;
-	ret = stse_set_default_handler_value(&stse_handler);
-	if (ret != STSE_OK) {
-		LOG_ERR("Failed to set default handler value: %d", ret);
-		return -1;
+	const struct device *se = DEVICE_DT_GET(DT_NODELABEL(stsafe_1_20));
+	if (!device_is_ready(se)) {
+		LOG_ERR("STSAFE device not ready");
+		return -ENODEV;
 	}
 
-#ifdef CONFIG_STSAFE_A110
-	stse_handler.device_type = STSAFE_A110;
-#elif defined(CONFIG_STSAFE_A120)
-	stse_handler.device_type = STSAFE_A120;
-#endif
+	LOG_INF("STSAFE device is ready: %s", se->name);
+	stse_Handle_t *stse_handle = stsafe_get_handle(se);
+	if (!stse_handle) {
+		LOG_ERR("Could not acquire STSAFE handle");
+		return -EBUSY;
+	}
 
-	ret = stse_init(&stse_handler, NULL);
-	if (ret != STSE_OK) {
-		LOG_ERR("Failed to initialize STSafe handler: %d", ret);
-		return -1;
-	}
-	LOG_RAW("STSafe handler initialized successfully.\n\n");
-	print_perso_info(&stse_handler);
+	uint8_t echo[8] = {'t', 'e', 's', 't', 't', 'e', 's', 't'};
+	uint8_t echo_reply[sizeof(echo)] = {0};
+	ret = stse_device_echo(stse_handle, echo, echo_reply, sizeof(echo_reply));
 
-#ifdef CONFIG_STSAFE_A110
-	stsafea_host_key_slot_t host_key_slot = {0};
-	ret = stsafea_query_host_key(&stse_handler, &host_key_slot);
 	if (ret != STSE_OK) {
-		LOG_ERR("Failed to get host key slot: %d", ret);
-		return -1;
+		LOG_ERR("stse_device_echo failed: 0x%x. Function is probably locked", ret);
 	}
-	LOG_RAW("Host Key Slot: \n\tPresence Flag: %d \n\tC-MAC Counter: "
-		"0x%02X%02X%02X\n\n",
-		host_key_slot.key_presence_flag, host_key_slot.cmac_sequence_counter[0],
-		host_key_slot.cmac_sequence_counter[1], host_key_slot.cmac_sequence_counter[2]);
-#elif defined(CONFIG_STSAFE_A120)
-	stsafea_host_key_slot_v2_t host_key_slot_v2 = {0};
-	ret = stsafea_query_host_key_v2(&stse_handler, &host_key_slot_v2);
-	if (ret != STSE_OK) {
-		LOG_ERR("Failed to get host key slot v2: %d", ret);
-		return -1;
+
+	LOG_HEXDUMP_INF(echo_reply, sizeof(echo_reply), "Echo reply from STSAFE:");
+
+	print_perso_info(stse_handle);
+
+	if (stse_handle->device_type == STSAFE_A100) {
+		stsafea_host_key_slot_t host_key_slot = {0};
+		ret = stsafea_query_host_key(stse_handle, &host_key_slot);
+		if (ret != STSE_OK) {
+			LOG_ERR("Failed to get host key slot: %d", ret);
+			return -1;
+		}
+		LOG_RAW("Host Key Slot: \n\tPresence Flag: %d \n\tC-MAC Counter: "
+			"0x%02X%02X%02X\n\n",
+			host_key_slot.key_presence_flag, host_key_slot.cmac_sequence_counter[0],
+			host_key_slot.cmac_sequence_counter[1],
+			host_key_slot.cmac_sequence_counter[2]);
+	} else if (stse_handle->device_type == STSAFE_A120) {
+		stsafea_host_key_slot_v2_t host_key_slot_v2 = {0};
+		ret = stsafea_query_host_key_v2(stse_handle, &host_key_slot_v2);
+		if (ret != STSE_OK) {
+			LOG_ERR("Failed to get host key slot v2: %d", ret);
+			return -1;
+		}
+		LOG_RAW("Host Key Slot V2: \n\tPresence Flag: %d\n\tKey Type: %d\n\tC-MAC "
+			"Counter: "
+			"0x%02X%02X%02X%02X\n\n",
+			host_key_slot_v2.key_presence_flag, host_key_slot_v2.key_type,
+			host_key_slot_v2.cmac_sequence_counter[0],
+			host_key_slot_v2.cmac_sequence_counter[1],
+			host_key_slot_v2.cmac_sequence_counter[2],
+			host_key_slot_v2.cmac_sequence_counter[3]);
 	}
-	LOG_RAW("Host Key Slot V2: \n\tPresence Flag: %d\n\tKey Type: %d\n\tC-MAC Counter: "
-		"0x%02X%02X%02X%02X\n\n",
-		host_key_slot_v2.key_presence_flag, host_key_slot_v2.key_type,
-		host_key_slot_v2.cmac_sequence_counter[0],
-		host_key_slot_v2.cmac_sequence_counter[1],
-		host_key_slot_v2.cmac_sequence_counter[2],
-		host_key_slot_v2.cmac_sequence_counter[3]);
-#endif
 
 	return 0;
 }
